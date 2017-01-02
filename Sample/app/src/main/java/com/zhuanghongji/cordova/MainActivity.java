730305a183099f189680fadc22ed66c7f04c2814
zhuanghongji <zhuanghongjichina@gmail.com>
2017-01-02 17:00:09 +0800

    提交“Sample”
    将Cordova的Android部分拿出来，建一个新的Sample项目，并测试Camera插件功能通过

---
+++ Sample/app/src/main/java/com/zhuanghongji/cordova/MainActivity.java
@@ -0,0 +1,27 @@
+package com.zhuanghongji.cordova;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.apache.cordova.CordovaActivity;
+
+public class MainActivity extends CordovaActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        // enable Cordova apps to be started in the background
+        Bundle extras = getIntent().getExtras();
+        if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
+            moveTaskToBack(true);
+        }
+
+//        // Set by <content src="index.html" /> in config.xml
+//        loadUrl(launchUrl);
+
+        String cameraIndex = "file:///android_asset/test/camera/index.html";
+        loadUrl(cameraIndex);
+    }
+}
